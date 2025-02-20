a867d6f79d77c5d1480ba93d81ce8d1653f56afa
Alex Berman <alexander.berman@gu.se>
2025-02-20 14:56:00 +0100

    interact.py: Set log level to WARNING
    (instead of DEBUG)

--- bkos/interact.py
+++ bkos/interact.py
@@ -1,4 +1,6 @@
 import readline # noqa
+import logging
+import argparse
 
 from bkos import bot
 from bkos.ontology import UserInput
@@ -8,6 +10,11 @@ import bkos.hello_world.nlg
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--log-level", default=logging.WARNING)
+    args = parser.parse_args()
+
+    logging.getLogger().setLevel(args.log_level)
     resources = {
         'domain_class': bkos.hello_world.domain.HelloWorldDomain,
         'nlu': bkos.hello_world.nlu,
