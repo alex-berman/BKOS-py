6cdeb3ebf30dceb78e1709d7d42c49e9db36d406
Alex Berman <alexander.berman@gu.se>
2025-02-20 14:26:55 +0100

    hello_world NLG: Support some ICMs

--- bkos/hello_world/nlg.py
+++ bkos/hello_world/nlg.py
@@ -10,4 +10,15 @@ def generate(move):
         return 'Your income is below the threshold.'
     if move == Assert(Supports(IncomeBelowThreshold(), Not(LoanApplicationApproved()))):
         return 'If the applicant\'s income is too low, a loan cannot be obtained.'
+    if isinstance(move, ICM):
+        return generate_icm(move)
     raise Exception(f'Failed to generate move {move}')
+
+
+def generate_icm(move):
+    if move.level == acceptance:
+        if move.polarity == positive:
+            return 'OK.'
+    if move.level == understanding and move.polarity == negative:
+        return "Sorry, I don't understand."
+    raise Exception(f'Failed to generate ICM move {move}')
