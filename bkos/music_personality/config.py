2e5e6afcf75e0c6c9650a73f9fe4ba74f18fa775
Alex Berman <alexander.berman@gu.se>
2025-02-21 13:13:26 +0100

    Support CLI demo for music_personality

--- bkos/music_personality/config.py
+++ bkos/music_personality/config.py
@@ -13,13 +13,33 @@ resources = {
     'extraversion_model_bundle': {
         'model': MagicMock(),
         'scaler': MagicMock(),
-        'features': MagicMock(),
+        'features': ['energy_mean', 'danceability_mean', 'valence_mean', 'loudness_mean']
     },
     'explainer': MagicMock(),
 }
 
+resources['extraversion_model_bundle']['model'].predict_proba.return_value = [[0.4, 0.6]]
+resources['explainer'].global_coefficients.return_value = {
+    'energy_mean': -0.2,
+    'danceability_mean': 2,
+    'valence_mean': 0.5,
+    'loudness_mean': -1,
+}
+resources['explainer'].local_contributions.return_value = {
+    'energy_mean': -0.1,
+    'danceability_mean': -0.2,
+    'valence_mean': -0.05,
+    'loudness_mean': 0.1
+}
+
 session_data = {
     'case_info': {
+        'feature_values': {
+            'energy_mean': 0.5,
+            'danceability_mean': -0.1,
+            'valence_mean': -0.1,
+            'loudness_mean': 0.2
+        }
 
     }
 }
